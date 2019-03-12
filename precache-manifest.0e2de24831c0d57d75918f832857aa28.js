a5366e1ad7c1c12bd4f0e2b8326dda7257813b8b
mihaisitaru <mihai.sitaru@yahoo.com>
2019-03-12 21:51:36 +0200

    React app apartment list

---
+++ precache-manifest.0e2de24831c0d57d75918f832857aa28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "73a6acf816234d30e95c",
+    "url": "/static/js/main.72981690.chunk.js"
+  },
+  {
+    "revision": "b5a6ae73291b1a8ec805",
+    "url": "/static/js/2.e9b9cf0c.chunk.js"
+  },
+  {
+    "revision": "73a6acf816234d30e95c",
+    "url": "/static/css/main.beea2f98.chunk.css"
+  },
+  {
+    "revision": "6c11b86e804ca1a7106af470fbd6583b",
+    "url": "/index.html"
+  }
+];
