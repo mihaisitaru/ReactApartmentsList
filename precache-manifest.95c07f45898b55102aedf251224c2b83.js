edf49749895f29e6ffcd79fad6a0b3a24bd18f3e
Mihai Sitaru <mihai.sitaru@gmail.com>
2019-03-12 23:12:32 +0200

    Add files via upload

---
+++ precache-manifest.95c07f45898b55102aedf251224c2b83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c969b480c8f4e64a28b",
+    "url": "/ReactApartmentsList/static/js/runtime~main.28d7a311.js"
+  },
+  {
+    "revision": "70c0e016251d075fd09d",
+    "url": "/ReactApartmentsList/static/js/main.2cc373e0.chunk.js"
+  },
+  {
+    "revision": "b5a6ae73291b1a8ec805",
+    "url": "/ReactApartmentsList/static/js/2.e9b9cf0c.chunk.js"
+  },
+  {
+    "revision": "70c0e016251d075fd09d",
+    "url": "/ReactApartmentsList/static/css/main.df8eafcb.chunk.css"
+  },
+  {
+    "revision": "c434cffb6fd63148686afee195820a3e",
+    "url": "/ReactApartmentsList/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e2de24831c0d57d75918f832857aa28.js"
+  "/ReactApartmentsList/precache-manifest.95c07f45898b55102aedf251224c2b83.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ReactApartmentsList/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
