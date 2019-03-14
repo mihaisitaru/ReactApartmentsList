f117a1532f320a8bdeef5d87cec1e74e39f93727
Mihai Sitaru <mihai.sitaru@gmail.com>
2019-03-14 22:39:04 +0200

    Add files via upload

---
+++ precache-manifest.6e365f98fabf90755f3d170b5fd69b1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c969b480c8f4e64a28b",
+    "url": "/ReactApartmentsList/static/js/runtime~main.28d7a311.js"
+  },
+  {
+    "revision": "30e092833028a94fddcc",
+    "url": "/ReactApartmentsList/static/js/main.78a1c6f3.chunk.js"
+  },
+  {
+    "revision": "3f151c7d5ce3a9e4b6e1",
+    "url": "/ReactApartmentsList/static/js/2.4958a57c.chunk.js"
+  },
+  {
+    "revision": "30e092833028a94fddcc",
+    "url": "/ReactApartmentsList/static/css/main.df8eafcb.chunk.css"
+  },
+  {
+    "revision": "242c20cf70d1e6446ee60e93cafbb2dc",
+    "url": "/ReactApartmentsList/index.html"
+  }
+];
